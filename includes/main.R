d405079b7f3b197ea4a389b26fe99331928be5c3
Sebastian <wahani@gmail.com>
2018-03-20 17:07:50 +0100

    update

---
+++ includes/main.R
@@ -0,0 +1,18 @@
+#!/usr/bin/env r
+# Author: Sebastian Warnholz
+# Email: Sebastian.Warnholz@inwt-statistics.de
+
+source("/includes/validate-settings.R", local = new.env())
+source("/includes/register-dependencies.R", local = new.env())
+
+if (file.exists("DESCRIPTION")) {
+  cat("Looks like a package...\n"); Sys.sleep(1)
+  suppressPackageStartupMessages(stopifnot(require(devtools)))
+  devtools::install(dependencies = TRUE)
+  library(devtools::as.package(".")$package, character.only = TRUE)
+  main()
+} else if (file.exists("main.R")) {
+  source("main.R")
+} else {
+  system("less /includes/README")
+}

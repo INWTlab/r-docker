bb1c8146598e4cc5d5d7fae67a2ddd5899b7d25e
Sebastian <wahani@gmail.com>
2018-06-22 15:19:01 +0200

    update installPackage

--- includes/installPackage.R
+++ includes/installPackage.R
@@ -2,21 +2,6 @@
 # author: Sebastian Warnholz
 # email: sebastian.warnholz@inwt-statistics.de
 
-## configuration for docopt
-doc <- "Usage: install.R [-h] [-x] [PKGS...]
--h --help            show this help text
--x --usage           show help and short example usage"
-
-opt <- docopt(doc)
-
-if (opt$usage) {
-  cat(doc, "\n\n")
-  cat("where PKGS... is one or more packages.
-Examples:
-  install.R modules\n")
-  q("no")
-}
-
 # source("/includes/validate-settings.R")
 # source("/includes/register-dependencies.R")
 

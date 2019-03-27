76391f65d65671b38916739b2c3e57a9d818243e
Sebastian <wahani@gmail.com>
2019-03-27 16:43:53 +0100

    fix for missing repositories

--- r-base/validate-settings.R
+++ r-base/validate-settings.R
@@ -11,8 +11,22 @@ local({
     cat("LOCAL:", reposLocal <- getOption("repos")["LOCAL"], "\n")
     if (!grepl("mran", reposRemote)) stop("The remote repository has to be versioned!")
     if (!grepl("r-repo", reposLocal)) warning("Local repos is not set correctly.")
+  } else if (!is.null(repos <- getOption("repos"))) {
+    ind <- lapply(repos, function(r) try(open(url(r))))
+    ind <- lapply(ind, inherits, what = "try-error")
+    ind <- unlist(ind)
+    if (any(ind)) {
+      cat("\n")
+      cat("Clearing repositories; now using:\n")
+      for (entry in repos[!ind]) cat(entry, "\n")
+      options(repos = repos[!ind])
+    } else {
+      cat(" OK\n")
+    }
   } else {
     Sys.sleep(1)
     cat(" OK\n")
   }
 })
+
+

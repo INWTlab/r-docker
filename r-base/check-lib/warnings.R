a4613d24bed71e817c9cfaabf200c3c15977a088
Sebastian <wahani@gmail.com>
2019-05-10 13:04:46 +0200

    fix for latest devtools::check

--- r-base/check-lib/warnings.R
+++ r-base/check-lib/warnings.R
@@ -1,7 +1,9 @@
 modules::export("check")
 
 check <- function(res) {
-  relevantWarnings <- res$warnings[
-    !grepl("Non-standard license specification", res$warnings)]
-  if (length(relevantWarnings) > 0) stop("Check produced warnings.")
+  if (length(res$warnings) > 0) {
+    relevantWarnings <- res$warnings[
+      !grepl("Non-standard license specification", res$warnings)]
+    if (length(relevantWarnings) > 0) stop("Check produced warnings.")
+  }
 }

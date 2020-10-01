6dab5c43e254b76a18ea060d55f7a80346ac9cb2
Sebastian Warnholz <wahani@gmail.com>
2020-10-01 23:05:23 +0200

    another optimistic try

--- r-base/installAutomake.sh
+++ r-base/installAutomake.sh
@@ -2,19 +2,21 @@
 
 VERSION=${1}
 
-wget http://ftp.gnu.org/gnu/automake/automake-$VERSION.tar.gz
-tar -xvfz automake-$VERSION.tar.gz
-cd automake-$VERSION
-./configure --prefix=/usr/local/automake-$VERSION
-make
-make install
-cd ..
-rm automake-$VERSION.tar.gz
-rm -rf automake-$VERSION
+function install_automake() {
+    wget http://ftp.gnu.org/gnu/automake/automake-$VERSION.tar.gz \
+        && tar -xvf automake-$VERSION.tar.gz \
+        && cd automake-$VERSION \
+        && ./configure --prefix=/usr/local/automake-$VERSION \
+        && make \
+        && make install \
+        && cd .. \
+        && rm automake-$VERSION.tar.gz \
+        && rm -rf automake-$VERSION \
+        && rm -f /usr/local/bin/aclocal-$VERSION \
+        && rm -f /usr/local/bin/automake-$VERSION \
+        && ln -s /usr/local/automake-$VERSION/bin/aclocal-$VERSION /usr/local/bin/ \
+        && ln -s /usr/local/automake-$VERSION/bin/automake-$VERSION /usr/local/bin/ \
+        && echo "Installed automake in version $VERSION"
+}
 
-ln -s /usr/local/automake-$VERSION/bin/aclocal-$VERSION /usr/local/bin/
-ln -s /usr/local/automake-$VERSION/bin/automake-$VERSION /usr/local/bin/
-
-echo Installed automake in version $VERSION
-
-                    
+install_automake

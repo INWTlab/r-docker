f9c735c6ccc7d271aa9a8fbaec61f9f793cb48af
Sebastian Warnholz <wahani@gmail.com>
2020-10-02 09:56:01 +0200

    more stories on automake

--- r-base/installAutomake.sh
+++ r-base/installAutomake.sh
@@ -2,6 +2,12 @@
 
 VERSION=${1}
 
+if echo $VERSION | grep -q '\..*\..*'; then 
+    MAJOR_VERSION=${VERSION%.*}
+else 
+    MAJOR_VERSION=${VERSION}
+fi
+
 function install_automake() {
     wget http://ftp.gnu.org/gnu/automake/automake-$VERSION.tar.gz \
         && tar -xvf automake-$VERSION.tar.gz \
@@ -14,8 +20,8 @@ function install_automake() {
         && rm -rf automake-$VERSION \
         && rm -f /usr/local/bin/aclocal-$VERSION \
         && rm -f /usr/local/bin/automake-$VERSION \
-        && ln -s /usr/local/automake-$VERSION/bin/aclocal-$VERSION /usr/local/bin/ \
-        && ln -s /usr/local/automake-$VERSION/bin/automake-$VERSION /usr/local/bin/ \
+        && ln -s /usr/local/automake-$VERSION/bin/aclocal-$VERSION /usr/local/bin/aclocal-$MAJOR_VERSION \
+        && ln -s /usr/local/automake-$VERSION/bin/automake-$VERSION /usr/local/bin/automake-$MAJOR_VERSION \
         && echo "Installed automake in version $VERSION"
 }
 

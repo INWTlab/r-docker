8993ff39f5486f351b94cf0e1147dbc6103ec2dc
Sebastian Warnholz <wahani@gmail.com>
2022-04-29 09:07:50 +0200

    R 4.2.0

--- examples/rcmdcheck/scratch.R
+++ examples/rcmdcheck/scratch.R
@@ -1,4 +1,9 @@
 
 devtools::document()
-res <- devtools::check(".", error_on = "warning")
+res <- devtools::check(".", error_on = "never")
+LIB <- modules::use("../../r-base/check-lib")
+
+LIB$errors$check(res)
+LIB$warnings$check(res)
+LIB$notes$check(res)
 str(res)

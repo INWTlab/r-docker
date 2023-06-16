272f5306a4d46609366e8e934facaa89ee7234e4
Jan A <jan.abel@inwt-statistics.de>
2023-06-16 16:24:43 +0200

    add get_mirror_date.R script & switch repos from mran to posit-cran mirror

---
+++ r-base/get_mirror_date.R
@@ -0,0 +1,29 @@
+create_mirror_url <- function(date) {
+  paste0("https://packagemanager.posit.co/cran/", format(date, "%Y-%m-%d"))
+}
+
+check_if_mirror_is_available <- function(date) {
+  url <- create_mirror_url(date)
+  stat <- toString(attr(curlGetHeaders(paste0(url, "/src/contrib/PACKAGES")), "status"))
+  ifelse(stat == "200", TRUE, FALSE)
+}
+
+get_mirror_date <- function() {
+  r_version_date <- as.Date(paste(utils::sessionInfo()[["R.version"]][c("year", "month", "day")], collapse = "-"))
+
+  for (date in as.list(seq(r_version_date, by = -1, length.out = 5))) {
+    if (check_if_mirror_is_available(date)) {
+      return(date)
+    }
+  }
+}
+
+mirror_date <- get_mirror_date()
+
+if (is.null(mirror_date)) {
+  q(status = 1)
+}
+
+cat(create_mirror_url(mirror_date))
+
+q(status = 0)
